0489876d5a60b781da7d66becd5559c25352f9ed
alalimov <alalimov@student.codam.nl>
2026-01-27 04:34:47 +0100

    Removed C++ 20 dependency.

--- cpp-03/ex1/ft_score_analytics.cpp
+++ cpp-03/ex1/ft_score_analytics.cpp
@@ -2,17 +2,16 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
-#include <ranges>
 
 int main(int argc, char** argv)
 {
     std::cout << "=== Player Score Analytics ===" << std::endl;
     if (argc < 2)
-        std::cout << "No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ..." << std::endl;
+        std::cout << "No scores provided. Usage: a.out <score1> <score2> ..." << std::endl;
     else
     {
         std::vector<int> score_list;
-        int total_score = 0, score = 0;
+        int total_score = 0, score = 0, range = 0, max = 0, min = 0;
 
     while(*++argv)
         try
@@ -29,25 +28,18 @@ int main(int argc, char** argv)
          std::cout << score << ", ";
     std::cout << "]" << std::endl;
     total_score = std::accumulate(score_list.begin(), score_list.end(), 0);
+    max = *std::max_element(score_list.begin(), score_list.end());
+    min = *std::min_element(score_list.begin(), score_list.end());
     std::cout << "Total players: " << argc - 1 << std::endl;
     std::cout << "Total score: " << total_score << std::endl;
     std::cout << "Average score: " << total_score / score_list.size() << std::endl;
-    std::cout << "High score: " << *std::max_element(score_list.begin(), score_list.end()) << std::endl;
-    std::cout << "Low score: " << *std::min_element(score_list.begin(), score_list.end()) << std::endl;
-    auto rng = [&]() -> int
-    {
-        if (score_list.size() == 1)
-        {
-            return *std::ranges::max_element(score_list);
-        }
-        else
-        {
-            auto [min_it, max_it] = std::ranges::minmax_element(score_list);
-            return *max_it - *min_it;
-        }
-        return 0;
-    }();
-    std::cout << "Score range: " << rng << std::endl;
+    std::cout << "High score: " << max << std::endl;
+    std::cout << "Low score: " << min << std::endl;
+    if (score_list.size() == 1)
+        range = max;
+    else
+        range = max - min;
+    std::cout << "Score range: " << range << std::endl;
     }
     return 0;
 }
