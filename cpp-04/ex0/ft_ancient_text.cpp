3acdef233883f0c4c089a9400a991dbfee5f43f7
alalimov <alalimov@student.codam.nl>
2026-02-02 10:57:25 +0100

    Exercise complete

---
+++ cpp-04/ex0/ft_ancient_text.cpp
@@ -0,0 +1,45 @@
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+  const std::string fileName = "ancient_fragment.txt";
+  std::cout << "=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n" << std::endl;
+  std::cout << "Accessing Storage Vault: " << fileName << std::endl;
+  std::cout << "Connection established...\n" << std::endl;
+
+  std::fstream file;
+  std::string line;
+  int i = 1;
+  file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+  try
+  {
+    file.open(fileName);
+    std::cout << "RECOVERED DATA:" << std::endl;
+    try
+    {
+      while (std::getline(file, line))
+      {
+        std::cout << "[FRAGMENT " << i << "] " << line << std::endl;
+        i++;
+      }
+    }
+    catch (const std::ios_base::failure &e)
+    {
+      std::cout << std::endl;
+      std::cout << "Data recovery complete. Storage unit disconnected." << std::endl;
+    }
+    file.close();
+  }
+  catch (const std::ios_base::failure &e)
+  {
+      std::cerr << "Error: '" << fileName << "' is not a valid file" << std::endl;
+      std::cerr << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
