046382e43fb1a3860e54819704693e69e89bef91
alalimov <alalimov@student.codam.nl>
2026-02-02 14:05:24 +0100

    fixed issue and formatted output

--- cpp-04/ex2/ft_stream_management.cpp
+++ cpp-04/ex2/ft_stream_management.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+    std::string id, status;
+
+    std::cout << "=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n" << std::endl;
+    std::cout << "Input Stream active. Enter archivist ID: ";
+    std::cin >> id;
+    std::cout << "Input Stream active. Enter status report: ";
+    std::getline(std::cin.ignore(), status);
+    std::cout << "[STANDARD] Archive status from " << id << ": " << status << std::endl;
+    std::cerr << "[ALERT] System diagnostic: Communication channels verified" << std::endl;
+    std::cout << "[STANDARD] Data transmission complete\n" << std::endl;
+
+    std::cout << "Three-channel communication test successful." << std::endl;
     return 0;
 }
