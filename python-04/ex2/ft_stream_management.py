a2bd57b26a962f070fb755eb09d50d2bfe1944b6
alalimov <alalimov@student.codam.nl>
2026-02-02 13:53:22 +0100

    Exercise complete

--- python-04/ex2/ft_stream_management.py
+++ python-04/ex2/ft_stream_management.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+import sys
+
+if __name__ == "__main__":
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+    id = input("Input Stream active. Enter archivist ID: ")
+    status = input("Input Stream active. Enter status report: ")
+
+    str1 = "[STANDARD] Archive status from " + f"{id}: {status}"
+    str2 = "[ALERT] System diagnostic: Communication channels verified"
+    str3 = "[STANDARD] Data transmission complete"
+
+    sys.stdout.write(f"{str1}\n")
+    sys.stderr.write(f"{str2}\n")
+    sys.stdout.write(f"{str3}\n")
+
+    print("Three-channel communication test successful.")
