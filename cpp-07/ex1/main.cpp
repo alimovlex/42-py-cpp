2e81144bf42f92dd23a6f9c42fddd30113caef77
alalimov <alalimov@student.codam.nl>
2026-02-26 12:03:50 +0100

    Almost done.

--- cpp-07/ex1/main.cpp
+++ cpp-07/ex1/main.cpp
@@ -3,9 +3,37 @@
 //
 
 #include <iostream>
+#include "Deck.hpp"
 
 int main()
 {
-    std::cout << "";
+    std::vector<std::map<std::string, std::string>> cards = {
+            {
+            {"type", "spell"},
+            {"name", "Lightning Bolt"},
+            {"cost", std::to_string(3)},
+            {"rarity", "Common"},
+            {"effect_type", "damage"}
+            },
+            {
+            {"type", "artifact"},
+            {"name", "Mana Crystal"},
+            {"cost", std::to_string(2)},
+            {"rarity", "Common"},
+            {"durability", std::to_string(5)},
+            {"effect_type", "Permanent: +1 mana per turn"}
+            },
+            {
+            {"type", "creature"},
+            {"name", "Fire Dragon"},
+            {"cost", std::to_string(5)},
+            {"rarity", "Legendary"},
+            {"attack", std::to_string(7)},
+            {"health", std::to_string(5)},
+            {"effect_type", "Creature summoned to battlefield"}
+            }
+    };
+    std::cout << "=== DataDeck Deck Builder ===\n" << std::endl;
+    //Deck deck = Deck();
     return 0;
 }
