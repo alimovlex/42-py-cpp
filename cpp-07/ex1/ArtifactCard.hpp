ab4bb39190d0c814aff55d812825e3ed3d8adee1
alalimov <alalimov@student.codam.nl>
2026-03-02 23:09:02 +0100

    The class design is completed.

--- cpp-07/ex1/ArtifactCard.hpp
+++ cpp-07/ex1/ArtifactCard.hpp
@@ -8,5 +8,19 @@ class ArtifactCard : public Card
 {
 public:
 
+    std::string name, rarity, effect;
+    int cost, durability, current_durability;
+
+    int get_durability();
+    int get_current_durability();
+    std::string get_effect();
+
+    std::map<std::string, std::string> play(std::map<std::string, std::string> &game_state) override;
+    std::map<std::string, std::string> activate_ability();
+    void repair(int amount);
+    std::string toString();
+
+    ArtifactCard(std::string& name, int cost, std::string& rarity, int durability, std::string& effect):
+    Card(name, cost, rarity), name(name), cost(cost), rarity(rarity), durability(durability), effect(effect), current_durability(durability) {}
 };
 
